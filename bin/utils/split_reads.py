fc9751f540e77bf03cf15b3d561da55e13f73a54
Nicolas Servant <nservant@curie.fr>
2015-07-23 19:18:21 +0200

    udpate split_reads.py utility

--- bin/utils/split_reads.py
+++ bin/utils/split_reads.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 from glob import glob
 import shutil
-
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename")
@@ -22,19 +22,25 @@ try:
 except OSError:
     pass
 
-cmd = "split -l %d -d %s %s" % (
-    nlines, filename,
-    os.path.join(out, os.path.basename(filename)))
 
-retcode = subprocess.call(cmd.split())
+if filename.endswith('.gz'):
+    prefix = re.sub('((.fastq)|(.fq)).gz','_part',os.path.join(out, os.path.basename(filename)))
+    cmd = "zcat %s | split -l %d -d - %s" % (
+        filename, nlines, prefix)
+else:
+    prefix = re.sub('(.fastq)|(.fq)','_part',os.path.join(out, os.path.basename(filename)))
+    cmd = "split -l %d -d %s %s" % (
+        nlines, filename, prefix)
+
+retcode = subprocess.call(cmd, shell=True)
 if retcode != 0:
     print "split file failed with return code %d", retcode
     sys.exit(1)
 
-files = glob(os.path.join(out, os.path.basename(filename) + "*"))
+files = glob(os.path.join(out, prefix + "*"))
 files.sort()
 for ifile in files:
     shutil.move(ifile,
                 os.path.join(os.path.dirname(ifile),
                              os.path.basename(ifile)[-2:] + "_" +
-                             os.path.basename(ifile)[:-2]))
+                             os.path.basename(ifile)[:-7] + ".fastq"))
