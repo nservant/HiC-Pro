3175848073662b7fafa22267c198a3552f3746dd
Nicolas Servant <nservant@curie.fr>
2016-02-25 10:53:13 +0100

    bug in config file due to space

--- scripts/hic.inc.sh
+++ scripts/hic.inc.sh
@@ -31,7 +31,7 @@ abspath() {
 
 filter_config()
 {
-    sed -e 's/#.*//' | egrep '^[ \t]*[a-zA-Z_][a-zA-Z0-9_]*[ \t]*:?=' | sed -e 's/[ \t]*:=[ \t]*/ :=/' -e 's/[ \t][^:]*=[ \t]*/ =/' -e 's/\([^ \t]*\)=/\1 =/' | sort -u -k 1b,1
+    sed -e 's/#.*//' | egrep '^[ \t]*[a-zA-Z_][a-zA-Z0-9_]*[ \t]*:?=' | sed -e 's/[ \t]*:=[ \t]*/ :=/' -e 's/[ \t][^:]*=[ \t]*/ =/' -e 's/\([^ \t]*\)=/\1 =/' -e 's/ *$//g' | sort -u -k 1b,1
 }
 
 read_config()
