b44ceec0322fab9ce1b66d836f48999f21edd502
nservant <nicolas.servant@curie.fr>
2016-06-17 10:10:24 +0200

    bug in py mod version check for dev version

--- scripts/install/check_pythonlib.py
+++ scripts/install/check_pythonlib.py
@@ -11,7 +11,7 @@ import re
 
 def vcmp(version1, version2):
     def normalize(v):
-        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+        return [int(x) for x in re.sub(r'(\.0+)(\.dev)?.*$','', v).split(".")]
     return cmp(normalize(version1), normalize(version2))
 
 
