063799d8fa25362d5777dd7a8f2e2b91156feb20
nservant <nicolas.servant@curie.fr>
2016-11-10 18:50:15 +0100

    fix bug for capture HiC

--- scripts/merge_valid_interactions.sh
+++ scripts/merge_valid_interactions.sh
@@ -68,14 +68,7 @@ do
 	nbf=$(find -L ${IN_DIR}/${RES_FILE_NAME} -maxdepth 1 -name "*.validPairs" | wc -l)
 	if [[ $nbf == 0 ]]; then die "Error : no valid interaction files found in ${DATA_DIR}/${RES_FILE_NAME}."; fi
 
-	
-	## On Target
-        if [[ ! -z ${CAPTURE_TARGET} ]]; then
-	    echo "## Select valid interactions from capture target ..." >> ${LDIR}/merge_valid_interactions.log
-	    ${SCRIPTS}/onTarget.py -i ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs -t ${CAPTURE_TARGET} -p -v 1> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs_ontarget 2>>${LDIR}/merge_valid_interactions.log
-	    mv ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs_ontarget ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs
-	fi
-
+	## Rm duplicates
 	if [[ ${RM_DUP} == 0 ]]
 	then
 	    cat ${IN_DIR}/${RES_FILE_NAME}/*.validPairs > ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs
@@ -87,14 +80,23 @@ do
 	allcount=$(cat  ${IN_DIR}/${RES_FILE_NAME}/*.validPairs | wc -l)
 	allcount_rmdup=$(cat ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs | wc -l)
 	nbdup=$(( allcount-allcount_rmdup ))
-	echo -e $RES_FILE_NAME"\t"$nbdup >> ${LDIR}/merge_valid_interactions.log
 
-	## merge stat file
-	echo -e "valid_interaction\t"$allcount > ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
-	echo -e "valid_interaction_rmdup\t"$allcount_rmdup >> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
-	awk '$2 == $5{cis=cis+1; d=$6>$3?$6-$3:$3-$6; if (d<=20000){sr=sr+1}else{lr=lr+1}} $2!=$5{trans=trans+1}END{print "trans_interaction\t"trans"\ncis_interaction\t"cis"\ncis_shortRange\t"sr"\ncis_longRange\t"lr}' ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs >> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
+        ## merge stat file
+	echo -e $RES_FILE_NAME"\t"$nbdup >> ${LDIR}/merge_valid_interactions.log
+        echo -e "valid_interaction\t"$allcount > ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
+        echo -e "valid_interaction_rmdup\t"$allcount_rmdup >> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
+        awk '$2 == $5{cis=cis+1; d=$6>$3?$6-$3:$3-$6; if (d<=20000){sr=sr+1}else{lr=lr+1}} $2!=$5{trans=trans+1}END{print "trans_interaction\t"trans"\ncis_interaction\t"cis"\ncis_shortRange\t"sr"\ncis_longRange\t"lr}' ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs >> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
+
+       ## On Target                                                                                                                                                      
+        if [[ ! -z ${CAPTURE_TARGET} ]]; then
+            echo "## Select valid interactions from capture target ..." >> ${LDIR}/merge_valid_interactions.log
+            ${SCRIPTS}/onTarget.py -i ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs -t ${CAPTURE_TARGET} -p -v 1> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs_ontarget 2>>${LDIR}/merge_valid_interactions.log
+            mv ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs_ontarget ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs
+            allcount_ontarget=$(cat ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs | wc -l)
+	    echo -e "valid_interaction_ontarget\t"$allcount_ontarget >> ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs.mergestat
+	fi
 	
-	## Split valid interaction per haplotype
+	## Allele specific analysis
 	if [[ ! -z ${ALLELE_SPECIFIC_SNP} ]]; then
 	    echo "## Split valid interactions for allele specific maps ..." >> ${LDIR}/merge_valid_interactions.log
 	    ${SCRIPTS}/split_valid_interactions.py -i ${DATA_DIR}/${RES_FILE_NAME}/${RES_FILE_NAME}_allValidPairs -v >> ${LDIR}/merge_valid_interactions.log
